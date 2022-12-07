86f42c5c0a8ccb2b03352931ee3c10d8a83c28d0
Robb Knight <desk@rknight.me>
2022-12-07 22:04:41 +0000

    Update MB posts on home page

--- src/_data/micro.js
+++ src/_data/micro.js
@@ -8,10 +8,12 @@ module.exports = async function() {
     .then(json => {
       return {
         posts: json.items.slice(0, 5).map(p => {
+          const summary = p.content_html.replace(/<[^>]*>/g, '').replace(/\n\n/g, "\n").replace(/\n/g, " ")
           return {
             html: p.content_html,
             url: p.url,
             title: p.title,
+            summary: summary.length > 500 ? `${summary.slice(0, 500)}...` : summary,
             date: p.date_published.substring(0, 16).replace('T', ' '),
           }
         }),
