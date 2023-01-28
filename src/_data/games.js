7169addef9ac96d322b8160188004da011494e32
Robb Knight <desk@rknight.me>
2023-01-28 18:16:22 +0000

    Reorder games

--- src/_data/games.js
+++ src/_data/games.js
@@ -1,18 +1,18 @@
 const PLATFORMS = [
-    { "name": "Master System", "key": "sega", "color": "#0089CF" },
-    { "name": "N64", "key": "n64", "color": "" },
-    { "name": "GameCube", "key": "gamecube", "color": "#6A5FBB" },
-    { "name": "3DS", "key": "3ds", "color": "#D12228" },
-    { "name": "Wii", "key": "wii", "color": "#8B8B8B" },
-    { "name": "Switch", "key": "switch", "color": "#e60012" },
-    { "name": "Xbox", "key": "xbox", "color": "#107C10" },
-    { "name": "Xbox 360", "key": "xbox", "color": "#107C10" },
-    { "name": "Xbox One", "key": "xbox", "color": "#107C10" },
-    { "name": "PS1", "key": "ps1" },
-    { "name": "PS2", "key": "ps2", "color": "#003791" },
-    { "name": "PS3", "key": "ps3", "color": "#003791" },
+    { "name": "PS5", "key": "ps5", "color": "#003791" },
     { "name": "PS4", "key": "ps4", "color": "#003791" },
-    { "name": "PS5", "key": "ps5", "color": "#003791" }
+    { "name": "PS3", "key": "ps3", "color": "#003791" },
+    { "name": "PS2", "key": "ps2", "color": "#003791" },
+    { "name": "PS1", "key": "ps1" },
+    { "name": "Xbox One", "key": "xbox", "color": "#107C10" },
+    { "name": "Xbox 360", "key": "xbox", "color": "#107C10" },
+    { "name": "Xbox", "key": "xbox", "color": "#107C10" },
+    { "name": "Switch", "key": "switch", "color": "#e60012" },
+    { "name": "Wii", "key": "wii", "color": "#8B8B8B" },
+    { "name": "3DS", "key": "3ds", "color": "#D12228" },
+    { "name": "GameCube", "key": "gamecube", "color": "#6A5FBB" },
+    { "name": "N64", "key": "n64", "color": "" },
+    { "name": "Master System", "key": "sega", "color": "#0089CF" },
 ]
 const GAMES = {
     "PS2": [
