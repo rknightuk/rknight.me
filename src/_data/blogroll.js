0f1a0c3e5a5c0b11e712f9a7f88c0d7116060e5e
Robb Knight <desk@rknight.me>
2023-11-25 14:51:06 +0000

    fixes

--- src/_data/blogroll.js
+++ src/_data/blogroll.js
@@ -16,7 +16,8 @@ const blogs = [
     { name: 'Matt Birchler', url: 'https://birchtree.me', feed: 'https://birchtree.me/rss/' },
     { name: 'Andrew Canion', url: 'https://canion.blog', feed: 'https://canion.blog/categories/article/feed.xml' },
     { name: 'Apple Annie', url: 'https://weblog.anniegreens.lol/', feed: 'https://weblog.anniegreens.lol/rss.xml' },
-    { name: 'Zach Leatherman', url: 'https://www.zachleat.com', feed: 'https://www.zachleat.com/web/feed.xml' },
+    { name: 'Zach Leatherman', url: 'https://www.zachleat.com', feed: 'https://www.zachleat.com/web/feed' },
+    { name: 'Marty Day', url: 'https://www.blast-o-rama.com', 'feed': 'https://www.blast-o-rama.com/feed/' },
 ]
 
 module.exports = async () => {
