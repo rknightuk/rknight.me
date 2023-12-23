f833a564605742056ba7fcd6f904a5bd34608149
Robb Knight <desk@rknight.me>
2023-12-23 12:15:47 +0000

    tina

---
+++ tina/__generated__/types.js
@@ -0,0 +1,183 @@
+export function gql(strings, ...args) {
+  let str = "";
+  strings.forEach((string, i) => {
+    str += string + (args[i] || "");
+  });
+  return str;
+}
+export const LinkPartsFragmentDoc = gql`
+    fragment LinkParts on Link {
+  __typename
+  title
+  link
+  date
+  comment
+}
+    `;
+export const PostPartsFragmentDoc = gql`
+    fragment PostParts on Post {
+  __typename
+  title
+  permalink
+  excerpt
+  layout
+  date
+  tags
+  body
+}
+    `;
+export const LinkDocument = gql`
+    query link($relativePath: String!) {
+  link(relativePath: $relativePath) {
+    ... on Document {
+      _sys {
+        filename
+        basename
+        breadcrumbs
+        path
+        relativePath
+        extension
+      }
+      id
+    }
+    ...LinkParts
+  }
+}
+    ${LinkPartsFragmentDoc}`;
+export const LinkConnectionDocument = gql`
+    query linkConnection($before: String, $after: String, $first: Float, $last: Float, $sort: String, $filter: LinkFilter) {
+  linkConnection(
+    before: $before
+    after: $after
+    first: $first
+    last: $last
+    sort: $sort
+    filter: $filter
+  ) {
+    pageInfo {
+      hasPreviousPage
+      hasNextPage
+      startCursor
+      endCursor
+    }
+    totalCount
+    edges {
+      cursor
+      node {
+        ... on Document {
+          _sys {
+            filename
+            basename
+            breadcrumbs
+            path
+            relativePath
+            extension
+          }
+          id
+        }
+        ...LinkParts
+      }
+    }
+  }
+}
+    ${LinkPartsFragmentDoc}`;
+export const PostDocument = gql`
+    query post($relativePath: String!) {
+  post(relativePath: $relativePath) {
+    ... on Document {
+      _sys {
+        filename
+        basename
+        breadcrumbs
+        path
+        relativePath
+        extension
+      }
+      id
+    }
+    ...PostParts
+  }
+}
+    ${PostPartsFragmentDoc}`;
+export const PostConnectionDocument = gql`
+    query postConnection($before: String, $after: String, $first: Float, $last: Float, $sort: String, $filter: PostFilter) {
+  postConnection(
+    before: $before
+    after: $after
+    first: $first
+    last: $last
+    sort: $sort
+    filter: $filter
+  ) {
+    pageInfo {
+      hasPreviousPage
+      hasNextPage
+      startCursor
+      endCursor
+    }
+    totalCount
+    edges {
+      cursor
+      node {
+        ... on Document {
+          _sys {
+            filename
+            basename
+            breadcrumbs
+            path
+            relativePath
+            extension
+          }
+          id
+        }
+        ...PostParts
+      }
+    }
+  }
+}
+    ${PostPartsFragmentDoc}`;
+export function getSdk(requester) {
+  return {
+    link(variables, options) {
+      return requester(LinkDocument, variables, options);
+    },
+    linkConnection(variables, options) {
+      return requester(LinkConnectionDocument, variables, options);
+    },
+    post(variables, options) {
+      return requester(PostDocument, variables, options);
+    },
+    postConnection(variables, options) {
+      return requester(PostConnectionDocument, variables, options);
+    }
+  };
+}
+import { createClient } from "tinacms/dist/client";
+const generateRequester = (client, options) => {
+  const requester = async (doc, vars, options2) => {
+    let url = client.apiUrl;
+    if (options2?.branch) {
+      const index = client.apiUrl.lastIndexOf("/");
+      url = client.apiUrl.substring(0, index + 1) + options2.branch;
+    }
+    const data = await client.request({
+      query: doc,
+      variables: vars,
+      url
+    });
+    return { data: data?.data, errors: data?.errors, query: doc, variables: vars || {} };
+  };
+  return requester;
+};
+export const ExperimentalGetTinaClient = () => getSdk(
+  generateRequester(
+    createClient({
+      url: "http://localhost:4001/graphql",
+      queries
+    })
+  )
+);
+export const queries = (client, options) => {
+  const requester = generateRequester(client, options);
+  return getSdk(requester);
+};
