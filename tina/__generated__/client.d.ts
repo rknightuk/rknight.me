106a2ae0b60e17250f788a2dfb962b7470a8990d
Robb Knight <desk@rknight.me>
2023-12-23 13:14:17 +0000

    tina

---
+++ tina/__generated__/client.d.ts
@@ -0,0 +1,5 @@
+import { createClient } from "tinacms/dist/client";
+import { queries } from "./types";
+export const client = createClient({ url: 'http://localhost:4001/graphql', token: '283467873bb0f3d43a77e5b61ff6b84d845d53a9', queries,  });
+export default client;
+  
