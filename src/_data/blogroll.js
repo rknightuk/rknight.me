c682c69e883f594a165cb0cad1937ea23427d848
Robb Knight <desk@rknight.me>
2023-12-30 21:42:56 +0000

    remove temp

--- src/_data/blogroll.js
+++ src/_data/blogroll.js
@@ -15,7 +15,7 @@ const blogs = [
     { name: 'Wade Urry', url: 'https://www.iwader.co.uk', feed: 'https://iwader.co.uk/feed.xml' },
     { name: 'fLaMEd', url: 'https://flamedfury.com', feed: 'https://flamedfury.com/feed.xml/' },
     { name: 'Matt Birchler', url: 'https://birchtree.me', feed: 'https://birchtree.me/rss/' },
-    { name: 'Andrew Canion', url: 'https://canion.blog', feed: 'https://canion.blog/categories/article/feed.xml' },
+    // { name: 'Andrew Canion', url: 'https://canion.blog', feed: 'https://canion.blog/categories/article/feed.xml' },
     { name: 'Apple Annie', url: 'https://weblog.anniegreens.lol/', feed: 'https://weblog.anniegreens.lol/rss.xml' },
     { name: 'Zach Leatherman', url: 'https://www.zachleat.com', feed: 'https://www.zachleat.com/web/feed' },
     { name: 'Marty Day', url: 'https://www.blast-o-rama.com', 'feed': 'https://www.blast-o-rama.com/feed/' },
