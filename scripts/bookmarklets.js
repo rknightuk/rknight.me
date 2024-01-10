8cae60b35894a8506329b23bde3d69b3fa0bfe38
Robb Knight <desk@rknight.me>
2024-01-10 14:35:26 +0000

    bookmarklets

---
+++ scripts/bookmarklets.js
@@ -0,0 +1,30 @@
+const fs = require('fs')
+const { parseHTML } = require('linkedom')
+
+const BOOKMARKLETS = [
+    'new-link',
+    'new-post',
+]
+
+const run = async () => {
+    let links = []
+
+    BOOKMARKLETS.forEach(b => {
+        const html = fs.readFileSync(`./tmp/${b}.html`, 'utf-8')
+        links.push(html.split('\n')[20])
+    })
+
+    const content = `---
+title: Bookmarklets
+subtitle: You found a secret page! There's nothing exciting here though, sorry.
+permalink: /bookmarklets/index.html
+layout: base
+---
+
+${links.join('\n')}
+`
+
+    fs.writeFileSync('./src/pages/bookmarklets.md', content)
+}
+
+run()
