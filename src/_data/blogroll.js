ac760660430f4a64b5d806f45995c56c106764db
Robb Knight <desk@rknight.me>
2024-01-20 20:24:17 +0000

    new blog roll

--- src/_data/blogroll.js
+++ src/_data/blogroll.js
@@ -1,70 +1,112 @@
-const xml2Json = require('xml2json')
-const { AssetCache } = require("@11ty/eleventy-fetch")
-
-const blogs = [
-    // { name: 'Cory Dransfeldt', url: 'https://coryd.dev', feed: 'https://feedpress.me/coryd' },
-    // { name: 'Keenan', url: 'https://gkeenan.co/avgb', feed: 'https://gkeenan.co/avgb?format=rss' },
-    // { name: 'David Daaaaaarnes', url: 'https://darn.es', feed: 'https://darn.es/rss.xml' },
-    // { name: 'Jason Burk', url: 'https://grepjason.sh', feed: 'https://grepjason.sh/rss.xml' },
-    // { name: 'Zoe Aubert', url: 'https://zoeaubert.me', feed: 'https://zoeaubert.me/rss.xml' },
-    // { name: 'Sophie Koonin', url: 'https://localghost.dev', feed: 'https://localghost.dev/feed.xml' },
-    // { name: 'Martin Feld', url: 'https://loungeruminator.net', feed: 'https://loungeruminator.net/feed/' },
-    // { name: 'Joe Steel', url: 'https://joe-steel.com', feed: 'https://joe-steel.com/feed' },
-    // { name: 'Lewis Dale', url: 'https://lewisdale.dev', feed: 'https://lewisdale.dev/feed/' },
-    // { name: 'Jarrod Blundy', url: 'https://heydingus.net', feed: 'https://heydingus.net/feed.rss' },
-    // { name: 'Wade Urry', url: 'https://www.iwader.co.uk', feed: 'https://iwader.co.uk/feed.xml' },
-    // { name: 'fLaMEd', url: 'https://flamedfury.com', feed: 'https://flamedfury.com/feed.xml/' },
-    // { name: 'Matt Birchler', url: 'https://birchtree.me', feed: 'https://birchtree.me/rss/' },
-    // { name: 'Andrew Canion', url: 'https://canion.blog', feed: 'https://canion.blog/categories/article/feed.xml' },
-    // { name: 'Apple Annie', url: 'https://weblog.anniegreens.lol/', feed: 'https://weblog.anniegreens.lol/rss.xml' },
-    // { name: 'Zach Leatherman', url: 'https://www.zachleat.com', feed: 'https://www.zachleat.com/web/feed' },
-    // { name: 'Marty Day', url: 'https://www.blast-o-rama.com', feed: 'https://www.blast-o-rama.com/feed/' },
-    // { name: 'Adam Newbold', url: 'https://notes.neatnik.net', feed: 'https://notes.neatnik.net/rss.xml' },
-]
-
-module.exports = async () => {
-    console.log("Fetching blogroll data")
-    let asset = new AssetCache("blogroll_data")
-
-    if (asset.isCacheValid('1h'))
-    {
-        console.log("Returning blogroll data from cache" )
-        return await asset.getCachedValue()
-    }
-
-    const promises = blogs.map(async (blog) => {
-        const feed = await fetch(blog.feed)
-        const xml = await feed.text()
-        const data = await xml2Json.toJson(xml, { object: true })
-        if (data.feed?.entry) {
-            const firstPost = (Array.isArray(data.feed.entry) ? data.feed.entry : [data.feed.entry]).slice(0, 1)[0]
-            blogData = {
-                ...blog,
-                post: {
-                    title: firstPost.title,
-                    link: Array.isArray(firstPost.link) ? firstPost.id : firstPost.link.href,
-                }
-            }
-        } else {
-            const firstPost = (Array.isArray(data.rss.channel.item) ? data.rss.channel.item : [data.rss.channel.item]).slice(0, 1)[0]
-            blogData = {
-                ...blog,
-                post: {
-                    title: firstPost.title,
-                    link: firstPost.link,
-                }
-            }
+module.exports = () => {
+    return [
+        {
+            name: 'Cory Dransfeldt',
+            url: 'https://coryd.dev',
+            feed: 'https://feedpress.me/coryd',
+            description: 'A smart developer and probably the one person who tinkers with their website as much as I do.',
+        },
+        {
+            name: 'Keenan',
+            url: 'https://gkeenan.co/avgb',
+            feed: 'https://gkeenan.co/avgb?format=rss',
+            description: 'A very smart and handsome friend who has an incredible way with words.',
+        },
+        {
+            name: 'David Daaaaaarnes',
+            url: 'https://darn.es',
+            feed: 'https://darn.es/rss.xml',
+            description: 'Dave posts about front end web things including lots of web component things.',
+        },
+        {
+            name: 'Jason Grep',
+            url: 'https://grepjason.sh',
+            feed: 'https://grepjason.sh/rss.xml',
+            description: 'One third of Hemispheric Views, Jason has an excellent looking website and posts about games, podcasts, and fun hardware projects.',
+        },
+        {
+            name: 'Zoe Aubert',
+            url: 'https://zoeaubert.me',
+            feed: 'https://zoeaubert.me/rss.xml',
+            description: 'My friend and colleague who posts about all sorts including type systems, Warhammer, and Lego.',
+        },
+        {
+            name: 'Sophie Koonin',
+            url: 'https://localghost.dev',
+            feed: 'https://localghost.dev/feed.xml',
+            description: 'A developer who has an incredible website (check out the early 00s theme) and brings back the fun of the web.',
+        },
+        {
+            name: 'Martin Feld',
+            url: 'https://loungeruminator.net',
+            feed: 'https://loungeruminator.net/feed/',
+            description: 'One third of Hemispheric Views, Martin posts sporadically but they\'re always bangers.',
+        },
+        {
+            name: 'Joe Steel',
+            url: 'https://joe-steel.com',
+            feed: 'https://joe-steel.com/feed',
+            description: 'Joe is a VFX artist and blogs about various media things, most notable about how terrible the Apple TV is (it is terrible).',
+        },
+        {
+            name: 'Lewis Dale',
+            url: 'https://lewisdale.dev',
+            feed: 'https://lewisdale.dev/feed/',
+            description: 'Lewis is a fellow UK-based developer posting about development things as well as cycling.',
+        },
+        {
+            name: 'Jarrod Blundy',
+            url: 'https://heydingus.net',
+            feed: 'https://heydingus.net/feed.rss',
+            description: 'Jarrod posts primarily about Apple-related topics, has an perfect domain name, and a wonderful website.',
+        },
+        {
+            name: 'Wade Urry',
+            url: 'https://www.iwader.co.uk',
+            feed: 'https://iwader.co.uk/feed.xml',
+            description: 'A friend and colleague posting about programming-related things and the occasional recipe.',
+        },
+        {
+            name: 'fLaMEd',
+            url: 'https://flamedfury.com',
+            feed: 'https://flamedfury.com/feed.xml/',
+            description: 'Posts about the open web and related topics and just look at that website. So good.',
+        },
+        {
+            name: 'Matt Birchler',
+            url: 'https://birchtree.me',
+            feed: 'https://birchtree.me/rss/',
+            description: 'Matt is a blogger and YouTuber posting thoughtful things about technology, media, and everything in between.',
+        },
+        {
+            name: 'Andrew Canion',
+            url: 'https://canion.blog',
+            feed: 'https://canion.blog/categories/article/feed.xml',
+            description: 'One third of Hemispheric Views, Andrew posts about spreadsheets. And probably other things too.',
+        },
+        {
+            name: 'Apple Annie',
+            url: 'https://weblog.anniegreens.lol/',
+            feed: 'https://weblog.anniegreens.lol/rss.xml',
+            description: 'Annie went incredibly in-depth with microformats but also has great posts about other web-related things. Her site is the pinnacle of what can be done weblog.lol.',
+        },
+        {
+            name: 'Zach Leatherman',
+            url: 'https://www.zachleat.com',
+            feed: 'https://www.zachleat.com/web/feed',
+            description: 'The creator of Eleventy who posts excellent articles about Eleventy, web components, and everything in between.',
+        },
+        {
+            name: 'Marty Day',
+            url: 'https://www.blast-o-rama.com',
+            feed: 'https://www.blast-o-rama.com/feed/',
+            description: 'Marty is great at finding interesting articles and videos. Gorgeous website.'
+        },
+        {
+            name: 'Adam Newbold',
+            url: 'https://notes.neatnik.net',
+            feed: 'https://notes.neatnik.net/rss.xml',
+            description: 'The creator of omg.lol who has to write more stuff if I list him here.',
         }
-
-        if (!blogData.post.link.startsWith('http')) {
-            blogData.post.link = `${blog.url}${blogData.post.link}`
-        }
-
-        return blogData
-    })
-    const data = await Promise.all(promises)
-    
-    await asset.save(data, "json")
-
-    return data
+    ]
 }
