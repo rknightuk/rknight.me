f896b344572524241d9740e4cd561e61c45c1969
Robb Knight <desk@rknight.me>
2024-01-20 21:07:43 +0000

    Update podroll

--- src/_data/podroll.js
+++ src/_data/podroll.js
@@ -1,49 +1,74 @@
-const xml2Json = require('xml2json')
-const { AssetCache } = require("@11ty/eleventy-fetch")
-
-const pods = [
-    { name: 'Hemispheric Views', url: 'https://hemisphericviews.com/', feed: 'https://listen.hemisphericviews.com/rss' },
-    { name: 'Really Specific Stories', url: 'https://www.rsspod.net/', feed: 'https://www.rsspod.net/rss' },
-    { name: 'Connected', url: 'https://www.relay.fm/connected/', feed: 'https://www.relay.fm/connected/feed' },
-    { name: 'Top Four', url: 'https://www.relay.fm/topfour/', feed: 'https://www.relay.fm/topfour/feed' },
-    { name: 'Ungeniused', url: 'https://www.relay.fm/ungeniused/', feed: 'https://www.relay.fm/ungeniused/feed' },
-    { name: 'UK Triv', url: 'https://www.theincomparable.com/gameshow/uktriv/', feed: 'https://feeds.theincomparable.com/gameshow/uktriv' },
-    { name: 'Random Pursuit', url: 'https://www.theincomparable.com/gameshow/trivia/', feed: 'https://feeds.theincomparable.com/gameshow/trivia' },
-    { name: 'AppStories', url: 'https://appstories.net', feed: 'https://appstories.net/episodes/feed' },
-    { name: 'Defocused', url: 'https://www.theincomparable.com/defocused/', feed: 'https://feeds.theincomparable.com/defocused' },
-    { name: 'A Very Good (Audio) Blog', url: 'https://gkeenan.co/avgb/i-did-it-i-made-an-audio-blog-so-you-can-listen-in-your-car-without-driving-into-a-ditch', feed: 'https://gkeenan.co/avgab?format=rss' },
-]
-
-module.exports = async () => {
-    console.log("Fetching podroll data")
-    let asset = new AssetCache("podroll_data")
-
-    if (asset.isCacheValid('1h'))
+module.exports = () => {
+    return [
     {
-        console.log("Returning podroll data from cache" )
-        return await asset.getCachedValue()
-    }
-
-    const promises = pods.map(async (pod) => {
-        const feed = await fetch(pod.feed)
-        const xml = await feed.text()
-        const data = await xml2Json.toJson(xml, { object: true })
-        const firstPost = (Array.isArray(data.rss.channel.item) ? data.rss.channel.item : [data.rss.channel.item]).slice(0, 1)[0]
-        const showData = {
-            title: pod.name,
-            image: data.rss.channel['itunes:image'].href,
-            link: data.rss.channel.link,
-        }
-
-        return {
-            show: showData,
-            title: firstPost.title,
-            link: firstPost.link,
-        }
-    })
-    const data = await Promise.all(promises)
-    
-    await asset.save(data, "json")
-
-    return data
+        name: 'Hemispheric Views',
+        url: 'https://hemisphericviews.com/',
+        feed: 'https://listen.hemisphericviews.com/rss',
+        description: 'A show in theory about tech, in reality about everything else. Lovely Discord too.',
+        cover: 'hv.jpg'
+    },
+    {
+        name: 'Connected',
+        url: 'https://www.relay.fm/connected/',
+        feed: 'https://www.relay.fm/connected/feed' ,
+        description: 'A show about Apple but also about japes, and rickies, and nonsense.',
+        cover: 'connected.jpg'
+    },
+    {
+        name: 'A Very Good (Audio) Blog',
+        url: 'https://gkeenan.co/avgb/i-did-it-i-made-an-audio-blog-so-you-can-listen-in-your-car-without-driving-into-a-ditch',
+        feed: 'https://gkeenan.co/avgab?format=rss' ,
+        description: 'The compainion podcast to A Very Good Blog. Keenan has the best voice.',
+        cover: 'avgab.jpg'
+    },
+    {
+        name: 'Defocused',
+        url: 'https://www.theincomparable.com/defocused/',
+        feed: 'https://feeds.theincomparable.com/defocused' ,
+        description: 'A movie podcast where they attempt to review the movies they watch but mostly chat about weird parts of the movies instead.',
+        cover: 'defocused.jpg'
+    },
+    {
+        name: 'Really Specific Stories',
+        url: 'https://www.rsspod.net/',
+        feed: 'https://www.rsspod.net/rss' ,
+        description: 'An interview show with Martin Feld of Hemispheric Views fame.',
+        cover: 'rsspod.jpg'
+    },
+    {
+        name: 'Top Four',
+        url: 'https://www.relay.fm/topfour/',
+        feed: 'https://www.relay.fm/topfour/feed' ,
+        description: 'Ranking everything from Christmas songs to Hot Dogs',
+        cover: 'topfour.jpg'
+    },
+    {
+        name: 'Ungeniused',
+        url: 'https://www.relay.fm/ungeniused/',
+        feed: 'https://www.relay.fm/ungeniused/feed' ,
+        description: 'A show about weird Wikipedia articles.',
+        cover: 'ungeniused.jpg'
+    },
+    {
+        name: 'Random Pursuit',
+        url: 'https://www.theincomparable.com/gameshow/trivia/',
+        feed: 'https://feeds.theincomparable.com/gameshow/trivia' ,
+        description: 'Imagine Trivial Pursuit but with loads of different editions, no board, and ghost points.',
+        cover: 'randpursuit.jpg'
+    },
+    {
+        name: 'UK Triv',
+        url: 'https://www.theincomparable.com/gameshow/uktriv/',
+        feed: 'https://feeds.theincomparable.com/gameshow/uktriv' ,
+        description: 'Like Random Pursuit but it\'s Americans trying to answer questions from the UK edition of the game.',
+        cover: 'uktriv.jpg'
+    },
+    {
+        name: 'AppStories',
+        url: 'https://appstories.net',
+        feed: 'https://appstories.net/episodes/feed' ,
+        description: 'A podcast about apps. Bet you couldn\'t have guessed from the title.',
+        cover: 'appstores.jpg'
+    },
+]
 }
