1006ac9483dfd437566a958256d866dfd1c0f27c
Robb Knight <desk@rknight.me>
2024-02-21 21:12:20 +0000

    Remoce status

--- src/_data/status.js
+++
@@ -1,9 +0,0 @@
-const fetch = require('node-fetch')
-
-module.exports = async function() {
-    console.log("Fetching latest status")
-
-    const res = await fetch('https://api.omg.lol/address/robb/statuses/')
-    const status = await res.json()
-    return status.response.statuses[0]
-}
