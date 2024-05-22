35748591308942005fb46993596b8ba7a095526c
Robb Knight <desk@rknight.me>
2024-05-22 08:35:45 +0100

    Push this

--- config/filters/date.js
+++ config/filters/date.js
@@ -1,27 +1,34 @@
 const { DateTime } = require('luxon')
 const moment = require('moment')
 
+const nth = (d) => {
+    if (d > 3 && d < 21) return 'th';
+    switch (d % 10) {
+        case 1:  return 'st'
+        case 2:  return 'nd'
+        case 3:  return 'rd'
+        default: return 'th'
+    }
+}
+
 module.exports = {
     dateForFeed: (date) => {
-        return new Date(date).toISOString()
+        return date.toISOString()
     },
     isOldPost: (date) => {
-        return moment(date).isBefore(moment().subtract(4, 'years'))
+        const d = DateTime.fromISO(date.toISOString())
+            .setZone('Europe/London')
+
+        return DateTime.now().diff(d, 'years').years > 4
     },
     diffInYears: (date) => {
-        return moment().diff(moment(date), 'years')
+        const d = DateTime.fromISO(date.toISOString())
+            .setZone('Europe/London')
+
+        return Math.floor(DateTime.now().diff(d, 'years').years)
     },
     toDateTime: (date) => {
         const formatted = DateTime.fromISO(date)
-
-        const trail = (number) => {
-            return parseInt(number, 10) < 10 ? `0${number}` : number
-        }
-
-        return `${formatted.year}-${trail(formatted.month)}-${trail(formatted.day)} ${trail(formatted.hour)}:${trail(formatted.minute)}`
-    },
-    toDateTimeFromUnix: (date) => {
-        const formatted = DateTime.fromSeconds(parseInt(date, 10))
 
         const trail = (number) => {
             return parseInt(number, 10) < 10 ? `0${number}` : number
@@ -41,15 +48,6 @@ module.exports = {
         return [year, month, day].join('-');
     },
     postDate: (date) => {
-        const nth = (d) => {
-            if (d > 3 && d < 21) return 'th';
-            switch (d % 10) {
-                case 1:  return 'st'
-                case 2:  return 'nd'
-                case 3:  return 'rd'
-                default: return 'th'
-            }
-        }
         const d = new Date(date)
         const month = d.toLocaleString('default', { month: 'long' })
         const day = d.getDate()
@@ -59,55 +57,19 @@ module.exports = {
         return `${month} ${day}${nominal} ${year}`
     },
     postTime: (date) => {
-        let d = new Date(date)
-        let hour = d.getHours()
-        let minute = d.getMinutes()
-
-        if (hour < 10) hour = `0${hour}`
-        if (minute < 10) minute = `0${minute}`
-
-        return `${hour}:${minute}`
+        return DateTime.fromISO(date.toISOString())
+            .setZone('Europe/London')
+            .toFormat('HH:mm')
     },
     postDateNoYear: (date) => {
-        const nth = (d) => {
-            if (d > 3 && d < 21) return 'th';
-            switch (d % 10) {
-                case 1:  return 'st'
-                case 2:  return 'nd'
-                case 3:  return 'rd'
-                default: return 'th'
-            }
-        }
-        const d = new Date(date)
-        const month = d.toLocaleString('default', { month: 'long' })
-        const day = d.getDate()
-        const nominal = nth(d.getDate())
+        const d = DateTime.fromISO(date.toISOString())
+            .setZone('Europe/London')
 
-        return `${month} ${day}${nominal}`
+        return `${d.toFormat(`MMMM d`)}${nth(d.day)}`
     },
     toDateTimeForHCard: (date) => {
-        let d = new Date(date)
-        let month = '' + (d.getMonth() + 1)
-        let day = '' + d.getDate()
-        let year = d.getFullYear()
-        let hour = d.getHours()
-        let minute = d.getMinutes()
-        let seconds = d.getSeconds()
-
-        if (month.length < 2) month = '0' + month;
-        if (day.length < 2) day = '0' + day;
-
-        let output = [year, month, day].join('-');
-        return `${output} ${hour}:${minute}:${seconds}`
+        return DateTime.fromISO(date.toISOString())
+            .setZone('Europe/London')
+            .toFormat('yyyy-MM-dd HH:mm:ss')
     },
-    monthDay: (date) => {
-        let d = new Date(date)
-        let month = '' + (d.getMonth() + 1)
-        let day = '' + d.getDate()
-
-        if (month.length < 2) month = '0' + month;
-        if (day.length < 2) day = '0' + day;
-
-        return [day, month].join('-');
-    }
 }
