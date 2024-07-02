9e2d70b96f960b7937859b32afdd61e7312f10f3
Robb Knight <desk@rknight.me>
2024-07-02 12:29:15 +0100

    fix some dates

--- config/filters/date.js
+++ config/filters/date.js
@@ -1,5 +1,4 @@
 const { DateTime } = require('luxon')
-const moment = require('moment')
 
 const nth = (d) => {
     if (d > 3 && d < 21) return 'th';
@@ -28,33 +27,19 @@ module.exports = {
         return Math.floor(DateTime.now().diff(d, 'years').years)
     },
     toDateTime: (date) => {
-        const formatted = DateTime.fromISO(date)
-
-        const trail = (number) => {
-            return parseInt(number, 10) < 10 ? `0${number}` : number
-        }
-
-        return `${formatted.year}-${trail(formatted.month)}-${trail(formatted.day)} ${trail(formatted.hour)}:${trail(formatted.minute)}`
+        return DateTime.fromISO(date)
+            .toFormat('yyyy-MM-dd HH:mm:ss')
     },
     isoDateOnly: (date) => {
-        let d = new Date(date)
-        let month = '' + (d.getMonth() + 1)
-        let day = '' + d.getDate()
-        let year = d.getFullYear()
-
-        if (month.length < 2) month = '0' + month;
-        if (day.length < 2) day = '0' + day;
-
-        return [year, month, day].join('-');
+        return DateTime.fromISO(date)
+            .setZone('Europe/London')
+            .toFormat('yyyy-MM-dd')
     },
     postDate: (date) => {
-        const d = new Date(date)
-        const month = d.toLocaleString('default', { month: 'long' })
-        const day = d.getDate()
-        const nominal = nth(d.getDate())
-        const year = d.getFullYear()
+        const d = DateTime.fromISO(date.toISOString())
+            .setZone('Europe/London');
 
-        return `${month} ${day}${nominal} ${year}`
+        return `${d.toFormat(`d`)}${nth(d.day)} ${d.toFormat(`LLLL yyyy`)}`
     },
     postTime: (date) => {
         return DateTime.fromISO(date.toISOString())
@@ -66,10 +51,5 @@ module.exports = {
             .setZone('Europe/London')
 
         return `${d.toFormat(`MMMM d`)}${nth(d.day)}`
-    },
-    toDateTimeForHCard: (date) => {
-        return DateTime.fromISO(date.toISOString())
-            .setZone('Europe/London')
-            .toFormat('yyyy-MM-dd HH:mm:ss')
     },
 }
