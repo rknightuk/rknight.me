a41e377642fb60d06938f463ab0ff4e3be297ab9
Robb Knight <desk@rknight.me>
2024-10-17 09:23:50 +0100

    Forgot to push this, new home page and explore

--- src/_data/site/explore.js
+++ src/_data/site/explore.js
@@ -1,5 +1,136 @@
 module.exports = function() {
 	return {
+		'stuff': [
+			{
+				title: 'About',
+				link: '/about',
+				description: 'All about me',
+				icon: 'about',
+			},
+			{
+				title: 'Blog',
+				link: '/blog',
+				description: 'My long-form posts',
+				icon: 'blog',
+			},
+			{
+				title: 'Notes',
+				link: '/notes',
+				description: 'notes, toots, and shitposts',
+				icon: 'notes',
+			},
+			{
+				title: 'Links',
+				link: '/links',
+				description: 'A collection of interesting links',
+				icon: 'links',
+			},
+			{
+				title: 'Almanac',
+				link: '/almanac',
+				description: 'A media blog - everything I\'ve watched, read, and played',
+				icon: 'almanac',
+			},
+			{
+				title: 'Everything',
+				link: '/explore/everything',
+				description: 'Every post from my blog, notes, links, Almanac, and changelogs',
+				icon: 'everything',
+			},
+			{
+				title: 'Subscribe',
+				link: '/subscribe',
+				description: 'Subscribe to my blog, notes, and links',
+				icon: 'rss',
+			},
+			{
+				title: 'Projects',
+				link: '/projects',
+				description: 'Everything I\'ve made',
+				icon: 'projects',
+			},
+			{
+				title: 'Intersect',
+				link: '/intersect',
+				description: 'A wiki of sorts. Everything I know, mostly',
+				icon: 'intersect',
+			},
+			{
+				title: 'Collections',
+				link: '/collections',
+				description: 'A catalog of my collections',
+				icon: 'collections',
+				more: [
+					{
+						title: 'Lego',
+						link: '/collections/lego',
+						description: 'My Lego collection',
+						icon: 'lego',
+					},
+					{
+						title: 'Games',
+						link: '/collections/games',
+						description: 'My game collection',
+						icon: 'games',
+					},
+					{
+						title: 'Comics',
+						link: '/collections/comics',
+						description: 'My comics collection',
+						icon: 'comics',
+					}
+				]
+			},
+			{
+				title: 'Podcasts',
+				link: '/podcasts',
+				description: 'My podcasts, my listening history, and a podroll',
+				icon: 'podcasts',
+			},
+			{
+				title: 'Log',
+				link: '/log',
+				description: 'Changelog and previous versions of this site',
+				icon: 'log',
+			},
+		],
+		slash: [
+			{
+				title: 'ai',
+				link: '/ai',
+				description: 'What I do and don\'t use AI for',
+			},
+			{
+				title: 'now',
+				link: '/now',
+				description: 'What I\'m up to now',
+			},
+			{
+				title: 'next',
+				link: '/next',
+				description: 'What I\'m up to next',
+			},
+			{
+				title: 'uses',
+				link: '/uses',
+				description: 'All the things I use on a day-to-day basis',
+			},
+			{
+				title: 'interests',
+				link: '/interests',
+				description: 'Things I\'m interested in, passionate about, or otherwise enjoy',
+			},
+			{
+				title: 'save',
+				link: '/save',
+				description: 'Use these referral links for services I use and we both get free stuff',
+			},
+			{
+				title: 'contact',
+				link: '/contact',
+				description: 'How to contact me',
+			},
+		],
 		'writing': [
 			{
 				title: 'Blog',
