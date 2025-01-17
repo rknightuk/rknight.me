e1e88d6cd7749f4517378db555295049a986610f
Robb Knight <desk@rknight.me>
2025-01-17 14:34:29 +0000

    Slash save baby

--- src/_data/site/save.js
+++ src/_data/site/save.js
@@ -2,53 +2,63 @@ module.exports = function() {
     return [
         { 
             name: 'omg.lol',
+            slug: 'omglol',
             link: 'https://home.omg.lol/referred-by/robb',
             desc: 'A cool domain (e.g. robb.omg.lol), email forwarding, profile page, pastebin, Mastodon instance and more',
         },
         { 
             name: 'Backblaze',
+            slug: 'backblaze',
             link: 'https://secure.backblaze.com/r/01gkf3',
             desc: 'Unlimited computer backup',
         },
         {
             name: 'Setapp',
-            link: 'https://go.setapp.com/invite/0jsvfx75',
-            desc: 'One month free',
+            slug: 'setapp',
+            link: 'https://setapp.sjv.io/c/4838493/2602804/5114',
+            desc: 'Extended 30 day trial with code <code>RKNIGHT</code>',
         },
         {
             name: 'Godspeed',
+            slug: 'godspeed',
             link: 'https://godspeedapp.com/a/RKNIGHT25',
             desc: 'Get 25% off your first 12 months',
         },
         { 
-            name: 'Hover',
-            link: 'https://hover.com/scHIv4WR',
-            desc: 'Most of my domains names are with Hover. $2 discount',
-        },
-        { 
             name: 'Fathom Analytics',
+            slug: 'fathom',
             link: 'https://usefathom.com/ref/IXCLSF',
             desc: 'Privacy-focused web analytics. $10 credit',
         },
         { 
             name: 'Bunny CDN',
+            slug: 'bunny',
             link: 'https://bunny.net?ref=b2i4y24apu',
-            desc: 'Fast, cheap CDN. $20 credit',
+            desc: 'Fast, cheap CDN.',
         },
         {
             name: 'Hetzner',
+            slug: 'hetzner',
             link: 'https://hetzner.cloud/?ref=Lt9D9KzKP6UQ',
             desc: 'Hosting. $5 credit',
         },
         { 
             name: 'Ooni',
+            slug: 'ooni',
             link: 'http://rwrd.io/4orgua6?c',
             desc: 'I love my Ooni pizza oven. 10% off',
         },
         {
             name: 'City Fibre',
+            slug: 'cityfibre',
             link: 'https://refer.cityfibre.com/robbk-13',
             desc: 'Fibre broadband - Get £25',
         },
+        {
+            name: 'Cleanshot X',
+            slug: 'cleanshot',
+            link: 'https://cleanshot.sjv.io/robb',
+            desc: 'The best screen capture tool for macOS',
+        }
     ]
 }
