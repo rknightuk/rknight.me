81c38ecf63db33e1be4bc10c62dde9de8d9e3d15
Robb Knight <desk@rknight.me>
2025-01-24 13:18:08 +0000

    Fix

--- src/_data/pixelfed.js
+++ src/_data/pixelfed.js
@@ -7,8 +7,6 @@ module.exports = async function() {
     console.log("Fetching pixelfed")
     let asset = new AssetCache("pixelfed")
 
-    return []
-
     if (asset.isCacheValid('1h'))
     {
         console.log("Returning pixelfed from cache" )
