221e956ec199ada570c7fed209e80e9a3267d3af
Robb Knight <desk@rknight.me>
2025-03-24 13:22:53 +0000

    Fixes

--- src/_data/catalog/games/platforms.js
+++ src/_data/catalog/games/platforms.js
@@ -1,18 +1,18 @@
 export default () => {
     return [
-        { name: 'PS5', key: 'ps5', color: 'white' },
-        { name: 'PS4', key: 'ps4', color: 'white' },
-        { name: 'PS3', key: 'ps3', color: 'white' },
-        { name: 'PS2', key: 'ps2', color: 'white' },
-        { name: 'PS1', key: 'ps1' },
-        { name: 'Xbox One', key: 'xbox', color: '#107C10' },
-        { name: 'Xbox 360', key: 'xbox', color: '#107C10' },
-        { name: 'Xbox', key: 'xbox', color: '#107C10' },
-        { name: 'Switch', key: 'switch', color: '#e60012' },
-        { name: 'Wii', key: 'wii', color: '#8B8B8B' },
-        { name: '3DS', key: '3ds', color: '#D12228' },
-        { name: 'GameCube', key: 'gamecube', color: '#6A5FBB' },
-        { name: 'N64', key: 'n64' },
-        { name: 'Master System', key: 'sega', color: '#0089CF' }
+        { name: 'PS5', class: 'ps5', key: 'ps5', color: 'white' },
+        { name: 'PS4', class: 'ps4', key: 'ps4', color: 'white' },
+        { name: 'PS3', class: 'ps3', key: 'ps3', color: 'white' },
+        { name: 'PS2', class: 'ps2', key: 'ps2', color: 'white' },
+        { name: 'PS1', class: 'ps1', key: 'ps1' },
+        { name: 'Xbox One', class: 'xbox-one', key: 'xbox', color: '#107C10' },
+        { name: 'Xbox 360', class: 'xbox-360', key: 'xbox', color: '#107C10' },
+        { name: 'Xbox', class: 'xbox', key: 'xbox', color: '#107C10' },
+        { name: 'Switch', class: 'switch', key: 'switch', color: '#e60012' },
+        { name: 'Wii', class: 'wii', key: 'wii', color: '#8B8B8B' },
+        { name: '3DS', class: 'threeds', key: '3ds', color: '#D12228' },
+        { name: 'GameCube', class: 'gamecube', key: 'gamecube', color: '#6A5FBB' },
+        { name: 'N64', class: 'n64', key: 'n64' },
+        { name: 'Master System', class: 'sega-ms', key: 'sega', color: '#0089CF' }
     ];
 };
