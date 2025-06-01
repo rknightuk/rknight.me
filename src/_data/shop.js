bf2284f7c93250da3795b126782bcbe1a67e65d7
Robb Knight <948436+rknightuk@users.noreply.github.com>
2025-06-01 08:46:11 +0100

    Update shop.js

--- src/_data/shop.js
+++ src/_data/shop.js
@@ -5,7 +5,7 @@ export default () => {
             image: 'https://cdn.rknight.me/shop/dont-at-me-stickers.jpg',
             description: "Two pack of Don't Know, Don't Care, Don't @ Me stickers. Shipped worldwide in June. Minimum $10 but you can pay more if you want.",
             id: 'buy_btn_1RRVcWGoUXgcrg9dcazETKla',
-            active: true,
+            active: false,
             link: '/shop/dont-at-me',
         }
     ]
