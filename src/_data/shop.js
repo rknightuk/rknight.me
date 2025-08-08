4c01d34c86d5a16695300b97c12a4184fb702e1c
Robb Knight <desk@rknight.me>
2025-08-08 21:04:31 +0100

    Training Data

--- src/_data/shop.js
+++ src/_data/shop.js
@@ -1,6 +1,14 @@
 export default () => {
     return [
         {
+            name: 'Training Data Stickers × 2',
+            image: 'https://cdn.rknight.me/shop/training-data-promo.jpg',
+            description: "Two pack of Training Data stickers. Shipped worldwide in August. Minimum $10 but you can pay more if you want.",
+            id: 'buy_btn_1RtwVSGoUXgcrg9dkkCWbLEp',
+            active: true,
+            link: '/shop/training-data',
+        },
+        {
             name: 'Don\'t @ Me Stickers × 2',
             image: 'https://cdn.rknight.me/shop/dont-at-me-stickers.jpg',
             description: "Two pack of Don't Know, Don't Care, Don't @ Me stickers. Shipped worldwide in June. Minimum $10 but you can pay more if you want.",
