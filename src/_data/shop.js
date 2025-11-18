a8466f1eb7560de09207bee3ebd527d14414d335
Robb Knight <desk@rknight.me>
2025-11-18 14:12:20 +0000

    Push this

--- src/_data/shop.js
+++ src/_data/shop.js
@@ -1,20 +1,28 @@
 export default () => {
     return [
         {
-            name: 'Training Data Stickers × 2',
-            image: 'https://cdn.rknight.me/shop/training-data-promo-2.jpg',
-            description: "Two pack of Training Data stickers. Shipped worldwide in August. $10.",
-            id: 'buy_btn_1RtwVSGoUXgcrg9dkkCWbLEp',
-            active: false,
-            link: '/shop/training-data',
+            name: "Don't Know Don't Care Don't @ Me pack",
+            image: 'https://cdn.rknight.me/shop/dont-at-me-four-pack.jpg',
+            description: "Four pack of Don't Know Don't Care Don't @ Me stickers. Shipped worldwide.",
+            id: 'buy_btn_1SUms4GoUXgcrg9dxTkMHg2R',
+            active: true,
+            link: 'https://buy.stripe.com/dRmdR90Ew2QKdfC4Uc2kw04',
         },
         {
-            name: 'Don\'t @ Me Stickers × 2',
-            image: 'https://cdn.rknight.me/shop/dont-at-me-stickers.jpg',
-            description: "Two pack of Don't Know, Don't Care, Don't @ Me stickers. Shipped worldwide in June. $10.",
-            id: 'buy_btn_1RRVcWGoUXgcrg9dcazETKla',
-            active: false,
-            link: '/shop/dont-at-me',
-        }
+            name: "Training Data double pack",
+            image: 'https://cdn.rknight.me/shop/piracy-pack.jpg',
+            description: "2 × Pirate Bay and 2 × Piracy Ad stickers. Shipped worldwide.",
+            id: 'buy_btn_1SUmteGoUXgcrg9dF5Qp06zD',
+            active: true,
+            link: 'https://buy.stripe.com/14AaEXaf6ezs7Vi86o2kw02',
+        },
+        {
+            name: "Pirate Bay Training Data pack",
+            image: 'https://cdn.rknight.me/shop/training-data-promo-2.jpg',
+            description: "A four pack of Pirate Bay Training Data stickers. Shipped worldwide.",
+            id: 'buy_btn_1SUmv2GoUXgcrg9dimg5ntZ7',
+            active: true,
+            link: 'https://buy.stripe.com/5kQeVdcnebng8ZmgCU2kw03',
+        },
     ]
 }
