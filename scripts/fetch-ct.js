ac1cfc464ffd154ba4db828848a02f0efaf61b65
Robb Knight <desk@rknight.me>
2026-01-09 16:12:10 +0000

    🎶

--- scripts/fetch-ct.js
+++ scripts/fetch-ct.js
@@ -5,20 +5,16 @@ const run = async () => {
     const data = await raw.json()
 
     data.items.forEach(item => {
-        const links = [
-            item.url,
-            item._song_details.apple_music_url,
-            item._song_details.songlink_url,
-        ].filter(l => l)
-
-        const content = `---
+        let content = `---
 title: "Crucial Track: ${item._song_details.song} by ${item._song_details.artist}"
 date: ${item.date_published.replace('000000Z', '000Z')}
 permalink: /almanac/music/ct${item.url.split('/').pop()}/index.html
-links: [${links.map(link => `'${link}'`).join(', ')}]
-poster: ${item._song_details.artwork_url}
 artist: ${item._song_details.artist}
 song: ${item._song_details.song}
+link: ${item.url}
+link_am: ${item._song_details.apple_music_url || ''}
+link_sl: ${item._song_details.songlink_url || ''}
+poster: ${item._song_details.artwork_url || ''}
 ---
 
 ${item._song_details.content}
