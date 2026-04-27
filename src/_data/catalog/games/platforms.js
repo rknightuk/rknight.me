cd7cc6448ee3a0fdd7e766ee95d8bc90d965e0b6
Robb Knight <desk@rknight.me>
2026-04-27 14:25:11 +0100

    Push this

--- src/_data/catalog/games/platforms.js
+++ src/_data/catalog/games/platforms.js
@@ -7,11 +7,9 @@ export default () => {
         { name: 'PS1', class: 'ps1', key: 'ps1' },
         { name: 'Xbox One', class: 'xbox-one', key: 'xbox', color: '#107C10' },
         { name: 'Xbox 360', class: 'xbox-360', key: 'xbox', color: '#107C10' },
-        { name: 'Xbox', class: 'xbox', key: 'xbox', color: '#107C10' },
         { name: 'Switch', class: 'switch', key: 'switch', color: '#e60012' },
         { name: 'Wii', class: 'wii', key: 'wii', color: '#8B8B8B' },
         { name: '3DS', class: 'threeds', key: '3ds', color: '#D12228' },
-        { name: 'GameCube', class: 'gamecube', key: 'gamecube', color: '#6A5FBB' },
         { name: 'N64', class: 'n64', key: 'n64' },
         { name: 'Master System', class: 'sega-ms', key: 'sega', color: '#0089CF' }
     ];
